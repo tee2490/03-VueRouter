62473df619a7ffb2ca726c3635f58a811199aca8
Teeradet <tee2490@hotmail.com>
2025-09-08 09:36:23 +0700

    12 create components

---
+++ src/components/Product/ProductDetail.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Product Detail</h1>
+  </div>
+</template>
---
+++ src/components/Product/ProductList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <p>Here is the list of products:</p>
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
+  </div>
+</template>
