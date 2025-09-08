67f3ea25fc1ad82055c00b6664ab0e07c6736024
Teeradet <tee2490@hotmail.com>
2025-09-08 10:46:39 +0700

    12 route with dynamic values

--- src/components/Product/ProductList.vue
+++ src/components/Product/ProductList.vue
@@ -2,10 +2,18 @@
   <div>
     <h1>Product List</h1>
     <p>Here is the list of products:</p>
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
+    <div v-for="product in products">
+      <router-link :to="`/product/${product.id}`">
+        {{ product.name }}</router-link
+      >
+    </div>
   </div>
 </template>
+
+<script setup>
+const products = [
+  { id: 1, name: "Product 1" },
+  { id: 2, name: "Product 2" },
+  { id: 3, name: "Product 3" },
+];
+</script>
