7d112017f0e0addb602b9cad624a37e7863856a3
Teeradet <tee2490@hotmail.com>
2025-09-08 11:21:25 +0700

    12 navigate from script to another component

--- src/components/Home/HomePage.vue
+++ src/components/Home/HomePage.vue
@@ -1,3 +1,14 @@
 <template>
   <div>Home Page</div>
+  <button @click="goToContact">Contact Us</button>
 </template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+function goToContact() {
+  //router.push({ name: "contact" });
+  router.push("/contact-us");
+}
+</script>
