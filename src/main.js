8863b7681f0fa62eca10100960f19d2f26a47cd4
Teeradet <tee2490@hotmail.com>
2025-09-08 09:40:54 +0700

    12 create router in vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./Router/routes.js";
+const app = createApp(App);
 
-createApp(App).mount("#app");
+app.use(router);
+app.mount("#app");
